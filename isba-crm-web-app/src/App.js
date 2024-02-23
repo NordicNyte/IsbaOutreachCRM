7c24bb68054b532bf7bfb1ca6a53593bc2da4b10
charesed <charesejd@gmail.com>
2024-02-22 20:33:38 -0800

    charese's nav
    navbar

--- isba-crm-web-app/src/App.js
+++ isba-crm-web-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* <Button> Test Button </Button> */}
         <p>
           Welcome to Isba Outreach! <code></code>
         </p>
