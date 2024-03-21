27e7024f774d7b20bb5ac9c35347af97e9a1dce1
NordicNyte <122410809+NordicNyte@users.noreply.github.com>
2024-03-21 15:38:27 -0700

    idrk

--- isba-crm-web-app/react-bootstrap-test/src/Home.js
+++ isba-crm-web-app/react-bootstrap-test/src/Home.js
@@ -8,44 +8,40 @@ import Modalpopup from './Modalpopup';
 
 export const Home = () => {
     return (
-    <body className='home-body'>
-        <div className="home-container">
-            <div className="center-content">
-                <div className="logo-and-heading">
-                    <img src={Logo} alt="Logo" width="150" height="150" /> {/* Include the logo here */}
-                    <h1 style={{ fontSize: '3em', fontWeight: 'bold', marginLeft: '10px' }}>ISBA Outreach</h1>
-                </div>
-                <p></p>
-            </div>
-            <Card className="custom-card">
-                <Card.Body className="card-body">
-                    <Card.Title></Card.Title>
-                    <Card.Text>
-                        Explore Connections
-                    </Card.Text>
-                    <div className="search-button-container">
-                        <Link to='/Modalpopup'>
-                            <Button variant="primary">Search</Button>
-                        </Link>
+        <div className='home-body'>
+            <div className="home-container">
+                <div className="center-content">
+                    <div className="logo-and-heading">
+                        <img src={Logo} alt="Logo" width="150" height="150" /> {/* Include the logo here */}
+                        <h1 style={{ fontSize: '3em', fontWeight: 'bold', marginLeft: '10px' }}>ISBA Outreach</h1>
                     </div>
-                </Card.Body>
-            </Card>
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col xs lg="2">
-                    </Col>
-                    <Col md="auto"></Col>
-                    <Col xs lg="2">
-
-                    </Col>
-                </Row>
-            </Container>
-        
-            
+                    <p></p>
+                </div>
+                <Card className="custom-card">
+                    <Card.Body className="card-body">
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            Explore Connections
+                        </Card.Text>
+                        <div className="search-button-container">
+                            <Link to='/Modalpopup'>
+                                <Button variant="primary">Search</Button>
+                            </Link>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Col xs lg="2">
+                        </Col>
+                        <Col md="auto"></Col>
+                        <Col xs lg="2">
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
-    </body>
-    )
+    );
 }
-
 
 export default Home;
