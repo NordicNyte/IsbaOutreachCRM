d3b4d06b9a988a59884d7456218be0c844df9cf5
rebekahrajadurai <rebekahrajadurai13@gmail.com>
2024-04-24 16:45:15 -0700

    build change

--- isba-crm-web-app/react-bootstrap-test/src/services/authService.js
+++ isba-crm-web-app/react-bootstrap-test/src/services/authService.js
@@ -1,30 +1,47 @@
 import { useState, useEffect } from "react";
-import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { signInWithPopup, OAuthProvider, signOut } from "firebase/auth";
 import { auth } from "../FirebaseConfig.js";
 
 export function SignIn() {
-  return (
-    <button onClick={() => signInWithPopup(auth, new GoogleAuthProvider())}>
-      SignIn
-    </button>
-  );
+  const signInWithMicrosoft = () => {
+    const provider = new OAuthProvider("microsoft.com");
+    // Add scopes for accessing Outlook data if needed
+    // provider.addScope('https://outlook.office.com/mail.read');
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // Handle successful sign-in
+      })
+      .catch((error) => {
+        // Handle errors
+      });
+  };
+
+  return <button onClick={signInWithMicrosoft}>SignIn with Outlook</button>;
 }
 
 export function SignOut() {
   return (
     <div>
-      Hello, {auth.currentUser.displayName} &nbsp;
-      <button onClick={() => signOut(auth)}>SignOut</button>
+      {auth.currentUser && (
+        <div>
+          Hello, {auth.currentUser.displayName} &nbsp;
+          <button onClick={() => signOut(auth)}>SignOut</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export function useAuthentication() {
   const [user, setUser] = useState(null);
+
   useEffect(() => {
-    return auth.onAuthStateChanged((user) => {
-      user ? setUser(user) : setUser(null);
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
     });
+
+    return () => unsubscribe();
   }, []);
+
   return user;
 }
